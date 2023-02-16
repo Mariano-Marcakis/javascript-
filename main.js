953b82a92cfff7e82cd617eb9788afe2dcc2551a
Mariano Marcakis <mariano.marcakis@gmail.com>
2023-02-16 17:57:48 -0300

    preentrega2

--- main.js
+++ main.js
@@ -2,12 +2,7 @@ let ingrseSuNombre = prompt("ingrese su nombre");
 function saludar(nombre) {
   alert("hola" + nombre);
 }
-/*let edad = prompt("Ingrese su edad");
-if (edad >= 18) {
-  saludar(ingrseSuNombre);
-} else {
-  alert("Eres menor de edad, vuelve cuando cumplas 18");
-}*/
+
 
 let edad = prompt("Ingrese su edad");
 let mayor = 18;
@@ -17,13 +12,35 @@ while (edad < 18) {
   alert("Vuelve cuando tengas 18");
   prompt("ingrese su edad");
 }
+/*function Producto (titulo, color, precio) {
+  this.titulo = titulo;
+  this.color = color;
+  this.precio = precio; 
+}
+const producto1 = new Producto("Remera slim","blanco", 3000);
+const producto2 = new Producto("Musculosa","Negra", 4500);
+console.log(producto1);
+console.log(producto2);
+*/
 
+var productos = [
+  {nombre: 'Musculosa deportiva', precio: 2000},
+  {nombre: 'Remera Overiceze', precio: 2500},
+  {nombre: 'Remera slim', precio: 3000}
+];
+
+productos.forEach((producto) => {
+  console.log(producto.nombre.precio)
+})
+alert("Mire y elija algun producto de la consola")
 let precio = prompt("Ingrese el precio del producto");
 let cantidad = prompt("Elija la cantidad del producto");
 
 alert("el subtotal a pagar  es:" + precio * cantidad);
 alert("El descuento es del 15%");
 alert("El precio a pagar en total con descuento es" + precio * cantidad * 0.85);
+
+
 
 // switch
 let diaDeLaSemana = Number(
@@ -57,4 +74,27 @@ switch (diaDeLaSemana) {
     alert("Ese día no existe");
     break;
 }
+const metodosPago = ["Tarjeta de crédito", "PayPal", "Transferencia bancaria", "Pago en efectivo"];
+ 
+let selectMetodoPago = document.createElement("select");
 
+for (let i = 0; i < metodosPago.length; i++) {
+    let option = document.createElement("option");
+  option.text = metodosPago[i];
+  selectMetodoPago.add(option);
+}
+
+document.body.appendChild(selectMetodoPago);
+
+
+/*
+
+var productos = [
+  {nombre: 'Musculosa deportiva', precio: 2000},
+  {nombre: 'Remera Overiceze', precio: 2500},
+  {nombre: 'Remera slim', precio: 3000}
+];
+
+productos.forEach((producto) => {
+  console.log(producto.nombre)
+}) */
